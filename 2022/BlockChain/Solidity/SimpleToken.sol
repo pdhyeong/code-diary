066ae97289fb17598a650a3f3aab9c8be4736e04
Dohyeong Park <71219602+pdhyeong@users.noreply.github.com>
2022-12-02 23:44:02 +0900

    Create SimpleToken.sol

---
+++ 2022/BlockChain/Solidity/SimpleToken.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+interface ERC20Interface {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transferFrom(address spender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Transfer(address indexed spender, address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 oldAmount, uint256 amount);
+}
+library SafeMath {
+  	function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a * b;
+		assert(a == 0 || c / a == b);
+		return c;
+  	}
+
+  	function div(uint256 a, uint256 b) internal pure returns (uint256) {
+	    uint256 c = a / b;
+		return c;
+  	}
+
+  	function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+		assert(b <= a);
+		return a - b;
+  	}
+
+  	function add(uint256 a, uint256 b) internal pure returns (uint256) {
+		uint256 c = a + b;
+		assert(c >= a);
+		return c;
+	}
+}
+abstract contract OwnerHelper {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed preOwner, address indexed nextOwner);
+
+    modifier onlyOwner {
+        require(msg.sender == _owner, "OwnerHelper: caller is not owner");
+        _;
+    }
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        require(newOwner != _owner);
+        require(newOwner != address(0x0));
+        address preOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(preOwner, newOwner);
+    }
+}
+contract SimpleToken is ERC20Interface, OwnerHelper {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) public _allowances;
+
+    uint256 public _totalSupply;
+    string public _name;
+    string public _symbol;
+    uint8 public _decimals;
+    bool public _tokenLock;
+
+    mapping (address => bool) public _personalTokenLock;
+    constructor(string memory getName, string memory getSymbol) {
+        _name = getName;
+        _symbol = getSymbol;
+        _decimals = 18;
+        _totalSupply = 100000000e18;
+        _balances[msg.sender] = _totalSupply; // 추가
+        _tokenLock = true;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() external view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint amount) public virtual override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view override returns (uint256) {
+        return _allowances[owner][spender]; // 소유자, 보내는 사람(사용할 사람) 주소로 
+    }
+
+    function approve(address spender, uint amount) external virtual override returns (bool) {
+        uint256 currentAllowance = _allowances[msg.sender][spender]; // 현재 사용할 수 있는 양
+        require(_balances[msg.sender] >= amount,"ERC20: The amount to be transferred exceeds the amount of tokens held by the owner.");
+        _approve(msg.sender, spender, currentAllowance, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external virtual override returns (bool) {
+        _transfer(sender, recipient, amount); // 기본적인 보내기 함수
+        emit Transfer(msg.sender, sender, recipient, amount); // 이번트 실행
+        uint256 currentAllowance = _allowances[sender][msg.sender]; // 현재 보낸사람이 사용할 수 있는 양지정
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        _approve(sender, msg.sender, currentAllowance, currentAllowance.sub(amount)); // 내 토큰을 사용할 수 있는 사람 지정
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        uint256 senderBalance = _balances[sender]; // 보낸사람의 잔고
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        _balances[sender] = senderBalance.sub(amount); // 보낸만큼 차감
+        _balances[recipient] += _balances[recipient].add(amount); // 받는사람은 추가
+    }
+    function _approve(address owner, address spender, uint256 currentAmount, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, currentAmount, amount);
+    }
+    function isTokenLock(address from,address to) public view returns (bool lock) {
+        lock = false;
+
+        if(_tokenLock == true) 
+        {
+            lock = true;
+        }
+        
+        if(_personalTokenLock[from] == true || _personalTokenLock[to] == true) {
+            lock = true;
+        }
+    }
+
+    function removeTokenLock() onlyOwner public { 
+        require(_tokenLock == true);
+        _tokenLock = false;
+    }
+
+    function removePersonalTokenLock(address _who) onlyOwner public {
+        require(_personalTokenLock[_who] == true);
+        _personalTokenLock[_who] = false;
+    }
+}
